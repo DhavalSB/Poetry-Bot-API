8c06f4b48c83f19c99c498f03dc5a01463a4d81c
Dhaval <pythondsb@gmail.com>
2024-06-11 11:55:43 -0700

    improved prompt quality

--- geminiInterface.py
+++ geminiInterface.py
@@ -11,15 +11,14 @@ model = genai.GenerativeModel('gemini-1.0-pro')
 
 generation_config=genai.types.GenerationConfig(
         candidate_count=1,
-        max_output_tokens=45,
-        temperature=0.72
+        temperature=1.0
 )
 
 def dailyPrompt():
 #Legacy    response = model.generate_content("a 2 line song lyric in quotes vaguely helpful for writing poetry, no extra words")
 
     response = model.generate_content(
-        contents = "write a short creative prompt in quotes, no extra words",
+        contents = "write something in 20 words that can inspire poetry, in quotes, no extra words",
         generation_config = generation_config
     )
     return response.text
