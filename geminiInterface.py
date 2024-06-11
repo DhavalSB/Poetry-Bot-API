3cd523526a782965944dfb158a1fffc0a7c467d6
Dhaval <pythondsb@gmail.com>
2024-06-10 17:05:13 -0700

    Better prompt quality, server now runs on localhost

--- geminiInterface.py
+++ geminiInterface.py
@@ -9,7 +9,17 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-1.0-pro')
 
+generation_config=genai.types.GenerationConfig(
+        candidate_count=1,
+        max_output_tokens=45,
+        temperature=0.72
+)
 
 def dailyPrompt():
-    response = model.generate_content("a 2 line song lyric in quotes vaguely helpful for writing poetry, no extra words")
+#Legacy    response = model.generate_content("a 2 line song lyric in quotes vaguely helpful for writing poetry, no extra words")
+
+    response = model.generate_content(
+        contents = "write a short creative prompt in quotes, no extra words",
+        generation_config = generation_config
+    )
     return response.text
