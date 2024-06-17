69c739fb2f2e6ed25cb6cbe7546b71e3f35a82e7
Dhaval <pythondsb@gmail.com>
2024-06-17 16:57:52 -0700

    improved prompt quality

--- geminiInterface.py
+++ geminiInterface.py
@@ -7,7 +7,7 @@ main.load_dotenv()
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-model = genai.GenerativeModel('gemini-1.0-pro')
+model = genai.GenerativeModel('gemini-1.5-pro')
 
 generation_config=genai.types.GenerationConfig(
         candidate_count=1,
@@ -18,7 +18,8 @@ def dailyPrompt():
 #Legacy    response = model.generate_content("a 2 line song lyric in quotes vaguely helpful for writing poetry, no extra words")
 
     response = model.generate_content(
-        contents = "write something in 20 words that can inspire poetry, in quotes, no extra words",
+#        contents = "write something about 20 words in length in quotes",
+        contents = "generate a poetry prompt in quotes only, but make it different",
         generation_config = generation_config
     )
     return response.text
